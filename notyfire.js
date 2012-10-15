b610f415db2a236c6d73e898fdd8aeaa6293a8ec
Aaron O'Mullan <aaron.omullan@friendco.de>
2012-10-16 00:16:54 +0200

    Initial commit

---
+++ notyfire.js
@@ -0,0 +1,64 @@
+/*
+ * NotyFire v2.0
+ * Copyright 2012 FriendCo.de
+ */
+
+
+// Modules imports
+var redisio = require("redis")
+var socketio = require('socket.io')
+
+exports.channels = {};
+
+exports.listen = function(server){
+    // Channels
+    exports.channels = {};
+
+    // Init message queue in Redis
+    var redis = redisio.createClient();
+    redis.subscribe('notyfire');
+    redis.on("message", function(channel, message) {
+        try{
+            message = JSON.parse(message);
+        }
+        catch (SyntaxError){return false;}
+        
+        for (var channel in message){
+            exports.transmit(channel,message[channel])
+        }
+    });
+
+    // Init socket.io
+    exports.io = socketio.listen(server);
+    exports.io.sockets.on('error', function (reason){
+        console.error('Unable to connect Socket.IO', reason);
+    });
+
+    exports.io.sockets.on('connection', function (socket) {
+        socket.on('subscribe', function (channel){
+            exports.subscribe(channel,socket);
+        });
+        socket.on('unsubscribe', function (chnnale){
+            exports.unsubscribe(channel,socket);
+        });
+        socket.on('disconnect', function () {
+            
+        });
+    });
+}
+
+/* Subscribe a socket to a channel */
+exports.subscribe = function(channel,socket){
+    socket.join(channel);
+};
+
+/* Unsubscribe a socket to a channel*/
+exports.unsubscribe = function(channel,socket){
+    socket.leave(channel);
+};
+
+
+/* Transmit data in channel */
+exports.transmit = function(channel,message){
+    exports.io.sockets.json.in(channel).emit(channel,message);
+}
---
+++ server.js
@@ -0,0 +1,9 @@
+/*
+ * NotyFire v2.0
+ * Copyright 2012 FriendCo.de
+ * 
+ * Example
+ */
+
+var notyfire = require("./notyfire.js");
+notyfire.listen(8001);
