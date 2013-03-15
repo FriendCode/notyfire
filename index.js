e2d0e270e743cdf19bb9b00cb81b280ce56ddc47
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-03-15 12:15:34 -0700

    Improve: Add Client

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var Notyfire = require('./lib/notyfire');
+var Notyfire = require('./lib/server');
+var NotyfireClient = require('./lib/client');
 
 // Utility function
 function createServer(port, server, channel, url) {
@@ -6,5 +7,10 @@ function createServer(port, server, channel, url) {
     return new Notyfire(port, server, channel, url);
 }
 
+function createClient(channel, port, server, redisOptions) {
+    return new NotyfireClient(channel, port, server, redisOptions);
+}
+
 // Exports
 exports.createServer = createServer;
+exports.createClient = createClient;
